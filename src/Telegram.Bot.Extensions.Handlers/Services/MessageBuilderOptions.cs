686db5d4052837ca0035a2228f112baf58c5fe78
Gleb Bannyy <gleb.bny@gmail.com>
2025-08-25 21:13:00 +0300

    REF: Refactor message builder options

--- src/Telegram.Bot.Extensions.Handlers/Services/MessageBuilderOptions.cs
+++ src/Telegram.Bot.Extensions.Handlers/Services/MessageBuilderOptions.cs
@@ -2,23 +2,19 @@ namespace LisBot.Common.Telegram.Services;
 
 public class MessageBuilderOptions
 {
-    public bool ClearOnBuild { get; init; }
-
     public int ButtonInRowCount { get; init; }
 
 
-    public MessageBuilderOptions(bool clearOnBuild, int buttonInRowCount)
+    public MessageBuilderOptions(int buttonInRowCount)
     {
         if(buttonInRowCount <= 0)
             throw new Exception("The Amount of buttons in one row should be higher than 1");
 
-        ClearOnBuild = clearOnBuild;
         ButtonInRowCount = buttonInRowCount;
     }
 
     public MessageBuilderOptions()
     {
-        ClearOnBuild = true;
         ButtonInRowCount = int.MaxValue;
     }
 }
