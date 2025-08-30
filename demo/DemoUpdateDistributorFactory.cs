fef047c0aa3b654baf023e23ae3a0a8af50c2ef9
Gleb Bannyy <gleb.bny@gmail.com>
2025-08-30 23:50:21 +0300

    FIX: Reflect Lib changes in demo

--- demo/DemoUpdateDistributorFactory.cs
+++ demo/DemoUpdateDistributorFactory.cs
@@ -1,6 +1,7 @@
 using LisBot.Common.Telegram.Builders;
 using LisBot.Common.Telegram.Factories;
 using LisBot.Common.Telegram.Services;
+using Telegram.Bot.Extensions.Handlers.Services.Markup;
 using Telegram.Bot.Extensions.Handlers.Services.Messaging;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
