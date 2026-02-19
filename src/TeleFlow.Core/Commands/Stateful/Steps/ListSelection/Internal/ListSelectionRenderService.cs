f9d003ac773d77e056ab88334a87b7a3f6780010
gleb <gleb.bny@gmail.com>
2026-02-19 21:45:01 +0300

    REF: Refactor ListSelection Step

---
+++ src/TeleFlow.Core/Commands/Stateful/Steps/ListSelection/Internal/ListSelectionRenderService.cs
@@ -0,0 +1,68 @@
+using TeleFlow.Core.Transport.Callbacks;
+using TeleFlow.Core.Transport.Markup;
+using Telegram.Bot.Types.ReplyMarkups;
+using static TeleFlow.Core.Transport.Callbacks.CallbackAction.Step;
+using static TeleFlow.Core.Transport.Callbacks.CallbackAction.Ui;
+
+namespace TeleFlow.Core.Commands.Stateful.Steps.ListSelection.Internal;
+
+internal static class ListSelectionRenderService
+{
+    internal static InlineKeyboardMarkup RenderMarkup<T>(ListSelectionCommandStepOptions<T> config, Func<CallbackAction, string> markupButtonActionCodec, ListSelectionCommandStepViewModel<T> vm)
+    {
+        IReadOnlyList<int> SelectedIndexes = vm.SelectedIndexes;
+
+        int cols = config.MaxItemsInRow;
+        int rows = config.MaxRowsInPage;
+        int pageSize = cols * rows;
+
+        int start = vm.Page * pageSize;
+        int count = vm.Values.Count;
+
+        if (start < 0) start = 0;
+        if (start >= count && count > 0)
+        {
+            vm.Page = (count - 1) / pageSize;
+            start = vm.Page * pageSize;
+        }
+
+        var b = new InlineKeyboardBuilder();
+
+        int idx = start;
+        for (int r = 0; r < rows && idx < count; r++)
+        {
+            for (int c = 0; c < cols && idx < count; c++, idx++)
+            {
+                T item = vm.Values[idx];
+                string text = config.DisplayNameParser(item);
+
+                if (config.Mode is ListSelectionMode<T>.Multi)
+                {
+                    if(SelectedIndexes.Contains(idx))
+                        text = '*' + text + '*';
+                }
+
+                b.ButtonCallback(text, markupButtonActionCodec(new SelectIndex(idx)));
+            }
+
+            b.NewRow();
+        }
+
+        bool hasPrev = vm.Page > 0;
+        bool hasNext = (start + pageSize) < count;
+
+        if (hasPrev || hasNext)
+        {
+            if (hasPrev)
+                b.ButtonCallback("<-", markupButtonActionCodec(new PrevPage()));
+            if (hasNext)
+                b.ButtonCallback("->", markupButtonActionCodec(new NextPage()));
+            b.NewRow();
+        }
+
+        if(config.Mode is ListSelectionMode<T>.Multi)
+            b.ButtonCallback("Done", markupButtonActionCodec(new Finish()));
+
+        return b.Build();
+    }
+}
--- src/TeleFlow.Core/Commands/Stateful/Steps/ListSelection/ListSelectionCommandStep.cs
+++ src/TeleFlow.Core/Commands/Stateful/Steps/ListSelection/ListSelectionCommandStep.cs
@@ -4,6 +4,7 @@ using TeleFlow.Abstractions.Engine.Commands.Stateful.Results;
 using TeleFlow.Abstractions.State.Step;
 using TeleFlow.Abstractions.Transport.Callbacks;
 using TeleFlow.Core.Commands.Stateful.Steps.CallbackStepBase;
+using TeleFlow.Core.Commands.Stateful.Steps.ListSelection.Internal;
 using TeleFlow.Core.Transport.Callbacks;
 using TeleFlow.Core.Transport.Markup;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -17,80 +18,24 @@ public class ListSelectionCommandStep<T> : CallbackCommandStepBase<ListSelection
     private readonly ListSelectionCommandStepOptions<T> _options;
     
     protected override async Task<ListSelectionCommandStepViewModel<T>> CreateDefaultViewModel(IServiceProvider sp)
-    {
-        var values = await _options.ValueProvider.Invoke(sp);
-        return new(values);
-    }
+        => new(await _options.ValueProvider.Invoke(sp));
 
     protected override InlineKeyboardMarkup RenderMarkup(Func<CallbackAction, string> markupButtonActionCodec, ListSelectionCommandStepViewModel<T> vm)
-    {
-        IReadOnlyList<int> SelectedIndexes = vm.SelectedIndexes;
+        => ListSelectionRenderService.RenderMarkup<T>(_options, markupButtonActionCodec, vm);
 
-        int cols = _options.MaxItemsInRow;
-        int rows = _options.MaxRowsInPage;
-        int pageSize = cols * rows;
-
-        int start = vm.Page * pageSize;
-        int count = vm.Values.Count;
-
-        if (start < 0) start = 0;
-        if (start >= count && count > 0)
-        {
-            vm.Page = (count - 1) / pageSize;
-            start = vm.Page * pageSize;
-        }
-
-        var b = new InlineKeyboardBuilder();
-
-        int idx = start;
-        for (int r = 0; r < rows && idx < count; r++)
-        {
-            for (int c = 0; c < cols && idx < count; c++, idx++)
-            {
-                T item = vm.Values[idx];
-                string text = _options.DisplayNameParser(item);
-
-                if (_options.Mode is ListSelectionMode<T>.Multi)
-                {
-                    if(SelectedIndexes.Contains(idx))
-                        text = '*' + text + '*';
-                }
-
-                b.ButtonCallback(text, markupButtonActionCodec(new SelectIndex(idx)));
-            }
-
-            b.NewRow();
-        }
-
-        bool hasPrev = vm.Page > 0;
-        bool hasNext = (start + pageSize) < count;
-
-        if (hasPrev || hasNext)
-        {
-            if (hasPrev)
-                b.ButtonCallback("<-", markupButtonActionCodec(new PrevPage()));
-            if (hasNext)
-                b.ButtonCallback("->", markupButtonActionCodec(new NextPage()));
-            b.NewRow();
-        }
-
-        if(_options.Mode is ListSelectionMode<T>.Multi)
-            b.ButtonCallback("Done", markupButtonActionCodec(new Finish()));
-
-        return b.Build();
-    }
 
     protected override async Task<CommandStepResult> HandleAction(IServiceProvider sp, StepState<ListSelectionCommandStepViewModel<T>> state, CallbackAction action)
     {
         return action switch
         {
             SelectIndex toggle => await HandleToggleAsync(sp, state, toggle.Index),
+            Finish => await HandleFinishAsync(sp, state),
             NextPage => await HandleNextPageAsync(sp, state),
             PrevPage => await HandlePrevPageAsync(sp, state),
-            Finish => await HandleFinishAsync(sp, state),
             _ => CommandStepResult.HoldOn(CommandStepHoldOnReason.InvalidInput, "Unsupported action."),
         };
     }
+
 
     private async Task<CommandStepResult> HandleToggleAsync(IServiceProvider sp, StepState<ListSelectionCommandStepViewModel<T>> state, int index)
     {
@@ -115,6 +60,19 @@ public class ListSelectionCommandStep<T> : CallbackCommandStepBase<ListSelection
         throw new Exception("Mode Unknown!");
     }
 
+    private async Task<CommandStepResult> HandleFinishAsync(IServiceProvider sp, StepState<ListSelectionCommandStepViewModel<T>> state)
+    {
+        if(_options.Mode is ListSelectionMode<T>.Multi multiSelectMode)
+        {
+            await multiSelectMode.OnCommit(new(sp), state.ViewModel.SelectedValues);
+            await FinalizeStep(sp);
+            return CommandStepResult.Next;
+        }
+        
+        throw new Exception("There should be no finish buttons!");
+    }
+
+
     private async Task<CommandStepResult> HandleNextPageAsync(IServiceProvider sp, StepState<ListSelectionCommandStepViewModel<T>> state)
     {
         var vm = state.ViewModel;
@@ -136,18 +94,6 @@ public class ListSelectionCommandStep<T> : CallbackCommandStepBase<ListSelection
 
         await UpsertAndRerender(sp, state);
         return CommandStepResult.HoldOn(CommandStepHoldOnReason.Other);
-    }
-
-    private async Task<CommandStepResult> HandleFinishAsync(IServiceProvider sp, StepState<ListSelectionCommandStepViewModel<T>> state)
-    {
-        if(_options.Mode is ListSelectionMode<T>.Multi multiSelectMode)
-        {
-            await multiSelectMode.OnCommit(new(sp), state.ViewModel.SelectedValues);
-            await FinalizeStep(sp);
-            return CommandStepResult.Next;
-        }
-        
-        throw new Exception("There should be no finish buttons!");
     }
 
 
