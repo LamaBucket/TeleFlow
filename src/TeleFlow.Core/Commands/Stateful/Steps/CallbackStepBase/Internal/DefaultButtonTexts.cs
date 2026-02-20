afc8eec305eaeba0805733078cf2211d3fba37b7
gleb <gleb.bny@gmail.com>
2026-02-20 23:37:57 +0300

    ADD: Create Default Button Texts

---
+++ src/TeleFlow.Core/Commands/Stateful/Steps/CallbackStepBase/Internal/DefaultButtonTexts.cs
@@ -0,0 +1,14 @@
+namespace TeleFlow.Core.Commands.Stateful.Steps.CallbackStepBase.Internal;
+
+internal static class DefaultButtonTexts
+{
+    internal static readonly string DoneButtonText = "Done";
+
+    internal static readonly string EmptyButtonText = " ";
+
+    internal static readonly string PrevPageButtonText = "<-";
+    internal static readonly string NextPageButtonText = "->";
+
+    internal static readonly string NextItemButtonText = ">>";
+    internal static readonly string PrevItemButtonText = "<<";
+}
