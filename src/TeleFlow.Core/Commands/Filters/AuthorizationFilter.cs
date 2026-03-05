ecdc1c74d480e6ed27e1c5851bda468a89142792
gleb <gleb.bny@gmail.com>
2026-03-05 18:48:16 +0300

    Add forbid message to auth filter

--- src/TeleFlow.Core/Commands/Filters/AuthorizationFilter.cs
+++ src/TeleFlow.Core/Commands/Filters/AuthorizationFilter.cs
@@ -2,6 +2,7 @@ using TeleFlow.Abstractions.Engine.Commands;
 using TeleFlow.Abstractions.Engine.Commands.Filters;
 using TeleFlow.Abstractions.Engine.Commands.Results;
 using TeleFlow.Abstractions.Engine.Pipeline.Contexts;
+using TeleFlow.Abstractions.Transport.Messaging;
 
 namespace TeleFlow.Core.Commands.Filters;
 
@@ -15,17 +16,26 @@ public class AuthorizationFilter : ICommandFilter
 
     private readonly AuthorizationFilterCommandResult _commandResultProvider;
 
+    private readonly string? _forbidMessage;
+
     public async Task<CommandResult> Execute(UpdateContext context, ICommandHandler next)
     {
         if(await _provider(context.ServiceProvider))
             return await next.Handle(context);
+
+        if(_forbidMessage is not null)
+        {
+            var msgService = context.GetService<IMessageSendService>();
+            await msgService.SendMessage(_forbidMessage);
+        }
         
         return _commandResultProvider();
     }
 
-    public AuthorizationFilter(AuthorizationProvider provider, AuthorizationFilterCommandResult? commandResultProvider = null)
+    public AuthorizationFilter(AuthorizationProvider provider, AuthorizationFilterCommandResult? commandResultProvider = null, string? forbidMessage = null)
     {
         _provider = provider;
         _commandResultProvider = commandResultProvider ?? (() => CommandResult.Exit);
+        _forbidMessage = forbidMessage;
     }
 }
