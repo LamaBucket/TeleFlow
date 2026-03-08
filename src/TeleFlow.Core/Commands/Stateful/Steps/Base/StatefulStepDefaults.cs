c6b9b957fdeac3a294602e515c5349f12f662b87
gleb <gleb.bny@gmail.com>
2026-03-08 11:32:34 +0300

    Move StatefulStepDefaults and CallbackButtonDefaultTexts to Toolkit project

--- src/TeleFlow.Core/Commands/Stateful/Steps/Base/StatefulStepDefaults.cs
+++
@@ -1,6 +0,0 @@
-namespace TeleFlow.Core.Commands.Stateful.Steps.Base;
-
-public static class StatefulStepDefaults
-{
-    public static readonly string StateExpiredMessage = "Your State Expired.";
-}
